46cd61864fc5fde763efd8bc9b92e43bff9c712c
Waquar1235 <90018402+Waquar1235@users.noreply.github.com>
2022-10-20 13:00:22 +0530

    Add files via upload

---
+++ lockedme/Main.java
@@ -0,0 +1,13 @@
+package com.project.lockedme;
+
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "D:\\Simplilearn\\lovkedMe"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+
+}
